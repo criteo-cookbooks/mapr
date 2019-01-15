f3b3aec965a391a0571aa3073825f0bd3f679601
m.benalla <m.benalla@criteo.com>
2019-01-15 16:52:16 +0100

    Fix issue with hadoop config merge

--- recipes/hadoop.rb
+++ recipes/hadoop.rb
@@ -1,14 +1,17 @@
 # Configure all the service from this point
-config = Mapr::AttributeMerger.new(node['mapr']['hadoop']['config'])
-config.merge(Mapr::NodeType.resourcemanager?, node['mapr']['hadoop']['resourcemanager']['config'])
-config.merge(Mapr::NodeType.storage?, node['mapr']['hadoop']['storage']['config'])
-config.merge(Mapr::NodeType.compute?, node['mapr']['hadoop']['compute']['config'])
 
 hadoop_config_path = ::File.join(node['mapr']['hadoop']['dir'], "hadoop-#{node['mapr']['hadoop']['version']}", 'etc', 'hadoop')
-config.hash.each do |config_name, local_config|
+
+node['mapr']['hadoop']['config'].each do |config_name, local_config|
+
+  config = Mapr::AttributeMerger.new(local_config)
+  config.merge(Mapr::NodeType.resourcemanager?, node['mapr']['hadoop']['resourcemanager']['config'][config_name])
+  config.merge(Mapr::NodeType.storage?, node['mapr']['hadoop']['storage']['config'][config_name])
+  config.merge(Mapr::NodeType.compute?, node['mapr']['hadoop']['compute']['config'][config_name])
+
   template File.join(hadoop_config_path, "#{config_name}-site.xml") do
     source 'conf.xml.erb'
-    variables(config: local_config)
+    variables(config: config.hash)
     owner node['mapr']['config']['owner']
     group node['mapr']['config']['group']
     mode node['mapr']['config']['mode']
